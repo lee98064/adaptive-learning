0e387b4de4a83c0e705bc66f9c5281c60a333397
Ryan Lee <lee98064@gmail.com>
2022-07-29 12:36:14 +0800

    Fix chart RWD and chart data

--- 學習歷程雷達/js/chartSetting.js
+++ 學習歷程雷達/js/chartSetting.js
@@ -1,4 +1,4 @@
-const chart_data = {
+const chart_default_data = {
   A: {
     answered: 0,
     notanswer: 0,
@@ -129,8 +129,76 @@ const chart_data = {
   },
 };
 
-const chart_options = [
-  {
+const chart_datasets = {
+  三大核心面向雷達圖: {
+    labels: ["自主行動", "社會參與", "溝工互動"],
+    datasets: [
+      {
+        label: "三大核心面向",
+        data: [0, 0, 0],
+        backgroundColor: "rgba(255, 177, 193, 0.5)",
+        borderColor: "rgba(251, 116, 144,0.8)",
+        borderWidth: 5,
+        fill: true,
+      },
+    ],
+  },
+  A: {
+    labels: ["A1", "A2", "A3"],
+    datasets: [
+      {
+        label: "自主行動",
+        data: [0, 0, 0],
+        backgroundColor: "rgba(255, 177, 193, 0.5)",
+        borderColor: "rgba(251, 116, 144,0.8)",
+        borderWidth: 5,
+        fill: true,
+      },
+    ],
+  },
+  B: {
+    labels: ["B1", "B2", "B3"],
+    datasets: [
+      {
+        label: "溝通互動",
+        data: [0, 0, 0],
+        backgroundColor: "rgba(255, 177, 193, 0.5)",
+        borderColor: "rgba(251, 116, 144,0.8)",
+        borderWidth: 5,
+        fill: true,
+      },
+    ],
+  },
+  C: {
+    labels: ["C1", "C2", "C3"],
+    datasets: [
+      {
+        label: "社會參與",
+        data: [0, 0, 0],
+        backgroundColor: "rgba(255, 177, 193, 0.5)",
+        borderColor: "rgba(251, 116, 144,0.8)",
+        borderWidth: 5,
+        fill: true,
+      },
+    ],
+  },
+  探究學習內容雷達圖: {
+    labels: ["發現問題", "規劃與研究", "論證與建模", "表達與分享"],
+    datasets: [
+      {
+        label: "探究學習內容",
+        data: [0, 0, 0, 0],
+        backgroundColor: "rgba(255, 177, 193, 0.5)",
+        borderColor: "rgba(251, 116, 144,0.8)",
+        borderWidth: 5,
+        fill: true,
+      },
+    ],
+  },
+};
+
+const chart_options = {
+  三大核心面向雷達圖: {
     legend: {
       display: false,
     },
@@ -139,6 +207,9 @@ const chart_options = [
         suggestedMin: 0,
         suggestedMax: 10,
         fontSize: 12,
+        max: 100,
+        min: 0,
+        stepSize: 10,
       },
       pointLabels: {
         fontSize: 12,
@@ -146,7 +217,7 @@ const chart_options = [
     },
     maintainAspectRatio: false,
   },
-  {
+  A: {
     legend: {
       display: false,
     },
@@ -155,6 +226,88 @@ const chart_options = [
         suggestedMin: 0,
         suggestedMax: 10,
         fontSize: 12,
+        max: 100,
+        min: 0,
+        stepSize: 10,
+      },
+      pointLabels: {
+        fontSize: 12,
+      },
+    },
+    maintainAspectRatio: false,
+  },
+  B: {
+    legend: {
+      display: false,
+    },
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 10,
+        fontSize: 12,
+        max: 100,
+        min: 0,
+        stepSize: 10,
+      },
+      pointLabels: {
+        fontSize: 12,
+      },
+    },
+    maintainAspectRatio: false,
+  },
+  C: {
+    legend: {
+      display: false,
+    },
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 10,
+        fontSize: 12,
+        max: 100,
+        min: 0,
+        stepSize: 10,
+      },
+      pointLabels: {
+        fontSize: 12,
+      },
+    },
+    maintainAspectRatio: false,
+  },
+  探究學習內容雷達圖: {
+    legend: {
+      display: false,
+    },
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 10,
+        fontSize: 12,
+        max: 100,
+        min: 0,
+        stepSize: 10,
+      },
+      pointLabels: {
+        fontSize: 12,
+      },
+    },
+    maintainAspectRatio: false,
+  },
+};
+
+const chart_options_768up = {
+  三大核心面向雷達圖: {
+    legend: {
+      display: false,
+    },
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 10,
+        fontSize: 15,
+        max: 100,
+        min: 0,
+        stepSize: 10,
       },
       pointLabels: {
         fontSize: 19,
@@ -162,7 +315,7 @@ const chart_options = [
     },
     maintainAspectRatio: false,
   },
-  {
+  A: {
     legend: {
       display: false,
     },
@@ -170,15 +323,18 @@ const chart_options = [
       ticks: {
         suggestedMin: 0,
         suggestedMax: 10,
-        fontSize: 12,
+        fontSize: 13,
+        max: 100,
+        min: 0,
+        stepSize: 10,
       },
       pointLabels: {
-        fontSize: 8,
+        fontSize: 19,
       },
     },
     maintainAspectRatio: false,
   },
-  {
+  B: {
     legend: {
       display: false,
     },
@@ -186,15 +342,18 @@ const chart_options = [
       ticks: {
         suggestedMin: 0,
         suggestedMax: 10,
-        fontSize: 12,
+        fontSize: 13,
+        max: 100,
+        min: 0,
+        stepSize: 10,
       },
       pointLabels: {
-        fontSize: 8,
+        fontSize: 19,
       },
     },
     maintainAspectRatio: false,
   },
-  {
+  C: {
     legend: {
       display: false,
     },
@@ -202,12 +361,34 @@ const chart_options = [
       ticks: {
         suggestedMin: 0,
         suggestedMax: 10,
-        fontSize: 18,
+        fontSize: 13,
+        max: 100,
+        min: 0,
+        stepSize: 10,
       },
       pointLabels: {
-        fontSize: 20,
+        fontSize: 19,
       },
     },
     maintainAspectRatio: false,
   },
-];
+  探究學習內容雷達圖: {
+    legend: {
+      display: false,
+    },
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 10,
+        fontSize: 15,
+        max: 100,
+        min: 0,
+        stepSize: 10,
+      },
+      pointLabels: {
+        fontSize: 19,
+      },
+    },
+    maintainAspectRatio: false,
+  },
+};
--- 學習歷程雷達/js/index.js
+++ 學習歷程雷達/js/index.js
@@ -2,80 +2,18 @@ var sTokenDataValue =
   "YzE2YTUzMjBmYTQ3NTUzMGQ5NTgzYzM0ZmQzNTZlZjVFZFVBZExZVG8wT250ek9qYzZJblZ6WlhKZmFXUWlPM002TVRRNklqRTVNREEwTVMxek1Ea3dNekF4SWp0ek9qZzZJbUpsYUdGMmFXOXlJanR6T2pFMU9pSmphR2x1WlhObFgzSmxZV1JwYm1jaU8zTTZPRG9pWTJ4cFpXNTBhV1FpTzNNNk16STZJalp1Y0hOYVVUUkxZbWh1VkRaUlZsZGhZa1JuVVdGaU9FdEdRMFE1WVdjMklqdHpPakV5T2lKamJHbGxiblJ6WldOeVpYUWlPM002TmpRNklsaHRiV05FUWxKNWNFVjNRbkJZY1UxTlpGbElXVGc0TW1obFoxaExhM05vVUhoU1YyNTJOMW95UkdOYVZVMVNPRGhJT1hWNVZsZHRjbGwzVm5VeWVIUWlPMzA9RWRVQWRMRjdCMUVDRjM5NzkxNDkzRUZGOTUzMEY0NDA5NjRGNTY==F7B1ECF39791493EFF9530F440964F56==";
 var proxy = "https://proxy.leetools.eu.org";
 
-var chart_list = [];
-
-const chart_datasets = [
-  {
-    labels: ["自主行動", "社會參與", "溝工互動"],
-    datasets: [
-      {
-        label: "三大核心面向",
-        data: [5, 5, 5],
-        backgroundColor: "rgba(255, 177, 193, 0.5)",
-        borderColor: "rgba(251, 116, 144,0.8)",
-        borderWidth: 5,
-        fill: true,
-      },
-    ],
-  },
-  {
-    labels: ["A1", "A2", "A3"],
-    datasets: [
-      {
-        label: "自主行動",
-        data: [84, 30, 70],
-        backgroundColor: "rgba(255, 177, 193, 0.5)",
-        borderColor: "rgba(251, 116, 144,0.8)",
-        borderWidth: 5,
-        fill: true,
-      },
-    ],
-  },
-  {
-    labels: ["符號運用與溝通表達", "科技資訊與媒體素養", "藝術涵養與美感素養"],
-    datasets: [
-      {
-        label: "溝通互動",
-        data: [0, 7, 3],
-        backgroundColor: "rgba(255, 177, 193, 0.5)",
-        borderColor: "rgba(251, 116, 144,0.8)",
-        borderWidth: 5,
-        fill: true,
-      },
-    ],
-  },
-  {
-    labels: ["道德實踐與公民意識", "人際關係與團隊合作", "多元文化與國際理解"],
-    datasets: [
-      {
-        label: "社會參與",
-        data: [6, 7, 8],
-        backgroundColor: "rgba(255, 177, 193, 0.5)",
-        borderColor: "rgba(251, 116, 144,0.8)",
-        borderWidth: 5,
-        fill: true,
-      },
-    ],
-  },
-  {
-    labels: ["發現問題", "規劃與研究", "論證與建模", "表達與分享"],
-    datasets: [
-      {
-        label: "社會參與",
-        data: [8, 7, 3, 8],
-        backgroundColor: "rgba(255, 177, 193, 0.5)",
-        borderColor: "rgba(251, 116, 144,0.8)",
-        borderWidth: 5,
-        fill: true,
-      },
-    ],
-  },
-];
+var chart_data = {};
 
 $(document).ready(async function () {
   // $.LoadingOverlay("show");
   // $.LoadingOverlay("hide");
   let getScienceLiteracyData = await getData("getStatistics_ScienceLiteracy");
+
+  chart_data = {
+    ...chart_default_data,
+    ...getScienceLiteracyData["data"],
+  };
+
   $("#scienceLiteracyStatistics_container").html(
     tmpl("scienceLiteracyStatistics_template", {
       data: {
@@ -83,28 +21,62 @@ $(document).ready(async function () {
         total_notanswer: getScienceLiteracyData["total_notanswer"],
         chtclass: getScienceLiteracyData["chtclass"],
         name: getScienceLiteracyData["name"],
-        scienceLiteracyData: {
-          ...chart_data,
-          ...getScienceLiteracyData["data"],
-        },
+        scienceLiteracyData: chart_data,
       },
     })
   );
+
+  drawRadarChart();
+
+  $(window, document).resize(function () {
+    drawRadarChart();
+  });
 
   $(".more").click(function (e) {
     $(this).siblings(".more-info").slideToggle("slow");
     $(this).children("i").toggleClass("active");
   });
 
-  $("[data-chart]").each(function (index, element) {
-    let temp = new Chart($(this), {
-      type: "radar",
-      data: chart_datasets[index],
-      options: chart_options[index],
-    });
-    chart_list.push(temp);
-  });
+  // $.LoadingOverlay("hide");
 });
+
+function drawRadarChart() {
+  $("[data-chart]").each(function (index, element) {
+    // 防止圖表變形
+    $(this).attr("style", "");
+    $(this).attr("heigth", "");
+    $(this).attr("width", "");
+
+    // 取得圖表名稱與資料
+    var chartName = $(this).data("chart");
+    var chartData = $(this).data("chartdata");
+
+    // 取得圖表設定
+    var options = chart_options_768up[chartName];
+    if ($(window).width() < 768) {
+      options = chart_options[chartName];
+    }
+
+    new Chart($(this), {
+      type: "radar",
+      data: getRadarData(chartName, chartData),
+      options: options,
+    });
+  });
+}
+
+function getRadarData(chartName, chartDataKey) {
+  chartDataKey = chartDataKey.split(",");
+
+  let data = [];
+  chartDataKey.forEach((key) => {
+    data.push(correctRateToNumber(chart_data[key].correct_rate));
+  });
+
+  let chartDataset = chart_datasets[chartName];
+  chartDataset["datasets"][0]["data"] = data;
+  return chartDataset;
+}
 
 async function getData(sFuncName, data = {}) {
   var vRtn = [];
